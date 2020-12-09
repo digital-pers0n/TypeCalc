f86ec3791503040418625683a5b16d711cf03dcf
digital-pers0n <aquaphonic@gmail.com>
2020-12-09 18:01:12 +0300

    Implement Calculator class

--- TypeCalc/TYPCalculator.m
+++ TypeCalc/TYPCalculator.m
@@ -9,6 +9,36 @@
 #import "TYPCalculator.h"
 #import "CalculatePrivate.h"
 
+@interface TYPCalculator () {
+    char _answer[1024];
+}
+
+@end
+
 @implementation TYPCalculator
+
+- (BOOL)validateExpression:(inout NSString **)string
+                     error:(out NSError **)outError
+{
+    if (*string == nil || (*string).length == 0) {
+        if (!outError) return NO;
+        *outError = [NSError errorWithDomain:NSCocoaErrorDomain
+                                        code:NSKeyValueValidationError
+                                    userInfo:nil];
+        return NO;
+    }
+    return YES;
+}
+
+- (NSString *)result {
+    int ret = CalculatePerformExpression(_expression.UTF8String,
+                                         /* significantDigits */ 100,
+                                         /* flags */ 1, _answer);
+    if (ret == 1) {
+        return @(_answer);
+    }
+    
+    return @"Invalid Expression.";
+}
 
 @end
