2e96f9ee30fe8837441107e38379ac871198e1a0
digital-pers0n <aquaphonic@gmail.com>
2020-12-10 17:50:40 +0300

    Implement WindowController class

--- TypeCalc/TYPWindowController.m
+++ TypeCalc/TYPWindowController.m
@@ -7,9 +7,11 @@
 //
 
 #import "TYPWindowController.h"
+#import "TYPCalculator.h"
 
 @interface TYPWindowController ()
-
+@property (nonatomic, readonly) TYPCalculator *calculator;
+@property (nonatomic) NSString *history;
 @end
 
 @implementation TYPWindowController
@@ -20,7 +22,18 @@
 
 - (void)windowDidLoad {
     [super windowDidLoad];
-    
+    _history = @"";
+    _calculator = [[TYPCalculator alloc] init];
+}
+
+- (IBAction)calculate:(id)sender {
+    NSMutableString *result = [[NSMutableString alloc] init];
+    [result appendString:_calculator.expression];
+    [result appendString:@" =\n"];
+    [result appendString:_calculator.result];
+    [result appendString:@"\n\n"];
+    [result appendString:_history];
+    self.history = result;
 }
 
 @end
